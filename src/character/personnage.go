b64391eb4047c2bd872cbf2fa608b505c5270668
Timothe <timothe.champieux@ynov.com>
2024-09-12 09:59:10 +0200

    creation personnage

--- src/character/personnage.go
+++ src/character/personnage.go
@@ -1,2 +1,18 @@
 package character
 
+type Daron struct {
+	name   string
+	hp     int
+	damage int
+}
+
+type Daronne struct {
+	name   string
+	hp     int
+	damage int
+}
+type Tonton struct {
+	name   string
+	hp     int
+	damage int
+}
