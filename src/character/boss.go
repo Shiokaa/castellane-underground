d4d1a68a1c9b5df7998d824d85e2b231620379ee
Amaru Tom <tom.amaru@ynov.com>
2024-09-12 09:58:15 +0200

    character

--- src/character/boss.go
+++ src/character/boss.go
@@ -1,1 +1,20 @@
 package character
+
+type Guetteur struct{
+	name string
+	hp int
+	damage int
+}
+
+type Vendeur struct{
+	name string
+	hp int
+	damage int
+}
+
+type Gofast struct{
+	name string
+	hp int
+	damage int
+}
+
