e167531f6060f716b425fe5cb769b36870a8afc3
romain <romain.bourdot@ynov.com>
2024-09-12 14:51:52 +0200

    inventory

--- src/inventory/sacoche.go
+++ src/inventory/sacoche.go
@@ -1,1 +1,17 @@
 package inventory
+
+import (
+	"fmt"
+	"projet-red/object"
+)
+
+type Inventory struct {
+	Weapon []object.Weapon
+	Drink  []object.Drink
+}
+
+func test() {
+	a := []int{1, 2, 3, 4, 5, 6}
+	a = append(a[:2], a[3:]...)
+	fmt.Printf(a)
+}
--- src/object/arme.go
+++ src/object/arme.go
@@ -1,13 +1,12 @@
 package object
 
 type Weapon struct {
-	Ceinture    int		/* -20 */
-	Claquette   int		/* -30 */
-	Boule       int		/* -5 ~ -200 */
-	Lacrimogène int		/* - 5% hp pdt 3 tours */
-	Mattraque   int		/* -80 */
-	Cocktail    int		/* -75 pdt 3 tours */
-	Taser       int		/* -100 + stun pdt 1 tour */
-	Mortier     int		/* -200 */
-	Poing       int		/* -10 */
+	Ceinture    int /* -20 */
+	Claquette   int /* -30 */
+	Boule       int /* -5 ~ -200 */
+	Lacrimogène int /* - 5% hp pdt 3 tours */
+	Mattraque   int /* -80 */
+	Cocktail    int /* -75 pdt 3 tours */
+	Taser       int /* -100 + stun pdt 1 tour */
+	Mortier     int /* -200 */
 }
