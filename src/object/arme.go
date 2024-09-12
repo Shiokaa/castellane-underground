e890fad5e17a23da206fd58f6276d88adfd5a79d
Amaru Tom <tom.amaru@ynov.com>
2024-09-12 14:56:37 +0200

    update

--- src/object/arme.go
+++ src/object/arme.go
@@ -1,13 +1,20 @@
 package object
 
+// Structure générique pour chaque arme
+type WeaponStats struct {
+    Name   string // Nom de l'arme
+    Damage int    // Dégâts infligés
+}
+
+// Structure principale pour les armes
 type Weapon struct {
-	Ceinture    int		/* -20 */
-	Claquette   int		/* -30 */
-	Boule       int		/* -5 ~ -200 */
-	Lacrimogène int		/* - 5% hp pdt 3 tours */
-	Mattraque   int		/* -80 */
-	Cocktail    int		/* -75 pdt 3 tours */
-	Taser       int		/* -100 + stun pdt 1 tour */
-	Mortier     int		/* -200 */
-	Poing       int		/* -10 */
+    Ceinture    WeaponStats // Ceinture (-20)
+    Claquette   WeaponStats // Claquette (-30)
+    Boule       WeaponStats // Boule (-5 ~ -200)
+    Lacrimogene WeaponStats // Lacrimogène (-5% hp pendant 3 tours)
+    Matraque    WeaponStats // Matraque (-80)
+    Cocktail    WeaponStats // Cocktail Molotov (-75 pdt 3 tours)
+    Taser       WeaponStats // Taser (-100 + stun pendant 1 tour)
+    Mortier     WeaponStats // Mortier (-200)
+	Poing       WeaponStats // Poing (-10)
 }
