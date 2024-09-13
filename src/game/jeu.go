770c03e121e8dbee892a515b590d18a13b9994e3
tomy-21 <tomyouadhi21@gmail.com>
2024-09-13 14:48:23 +0200

    updates

--- src/game/jeu.go
+++ src/game/jeu.go
@@ -9,7 +9,7 @@ import (
 
 func ChoixPersonnage() character.Personnage {
 	time.Sleep(3 * time.Second)
-	fmt.Println("\n\nChoisissez un personnage :\n\nDARON : 55 ans, fumeur, passe son temps au PMU, supporte l'OM plus que sa femme. 100 kg au compteur, se bat avec une ceinture. \n(Beaucoup de points de vie, dégâts faibles, apparaît avec 3 Ricards, 10 €, un briquet et un sandwich) TAPEZ 1\n\nDARONNE : 44 ans, femme de ménage, regarde Les Marseillais et TPMP tous les soirs, brune, 60 kg, lance des claquettes pour attaquer. \n(Dégâts élevés, peu de points de vie, apparaît avec 1 Ricard, 10 €, un briquet et un sandwich) TAPEZ 2\n\nTONTON : Tonton cool, il aime les femmes, la plage, les paris sportifs et la pétanque par-dessus tout, gagne sa vie au black et avec l'achat-revente de T-MAX volés,\nrêve d'une carrière de bouliste et ne se bat QU'AVEC SES BOULES UNIQUEMENT. Il est mauvais, mais avec un peu de chance, il peut infliger des dégâts énormes. \n(Points de vie moyens, dégâts aléatoires, apparaît avec 2 Ricards, entre 5 et 50 €, un briquet et un sandwich) TAPEZ 3")
+	fmt.Println("\n\nDARON : 55 ans, fumeur, passe son temps au PMU, supporte l'OM plus que sa femme. 100 kg au compteur, se bat avec une ceinture. \n(Beaucoup de points de vie, dégâts faibles) TAPEZ 1\n\nDARONNE : 44 ans, femme de ménage, regarde Les Marseillais et TPMP tous les soirs, brune, 60 kg, lance des claquettes pour attaquer. \n(Dégâts élevés, peu de points de vie) TAPEZ 2\n\nTONTON : Tonton cool, il aime les femmes, la plage, les paris sportifs et la pétanque par-dessus tout, gagne sa vie au black et avec l'achat-revente de T-MAX volés,\nrêve d'une carrière de bouliste et ne se bat QU'AVEC SES BOULES UNIQUEMENT. Il est mauvais, mais avec un peu de chance, il peut infliger des dégâts énormes. \n(Points de vie moyens, dégâts aléatoires) TAPEZ 3")
 	var choix_perso int
 	fmt.Scan(&choix_perso)
 
@@ -28,7 +28,7 @@ func ChoixPersonnage() character.Personnage {
 			fmt.Scan(&nameuser)
 		}
 		perso = character.Personnage{"Daron", nameuser, 120, 10, 20, 120}
-		fmt.Printf("Voici les stats du %v ( %v )\nPoints de vie : %v\nArgent : %v", perso.Name, perso.NameUser, perso.Hp, perso.Gold)
+		fmt.Printf("Voici les stats du %v %v \nPoints de vie : %v\nArgent : %v", perso.Name, perso.NameUser, perso.Hp, perso.Gold)
 	case 2:
 		fmt.Println("\nChoisisez un pseudo pour votre personnage \n")
 		fmt.Scan(&nameuser)
@@ -37,7 +37,7 @@ func ChoixPersonnage() character.Personnage {
 			fmt.Scan(&nameuser)
 		}
 		perso = character.Personnage{"Daronne", nameuser, 80, 10, 35, 120}
-		fmt.Printf("Voici les stats de la %v ( %v )\nPoints de vie : %v\nArgent : %v", perso.Name, perso.NameUser, perso.Hp, perso.Gold)
+		fmt.Printf("Voici les stats de la %v %v \nPoints de vie : %v\nArgent : %v", perso.Name, perso.NameUser, perso.Hp, perso.Gold)
 	case 3:
 		fmt.Println("\nChoisisez un pseudo pour votre personnage \n")
 		fmt.Scan(&nameuser)
@@ -48,7 +48,7 @@ func ChoixPersonnage() character.Personnage {
 		rand.Seed(time.Now().UnixNano())
 		argentrandom := rand.Intn(46) + 5
 		perso = character.Personnage{"Tonton", nameuser, 100, argentrandom, 20, 120}
-		fmt.Printf("\nVoici les stats du %v ( %v )\nPoints de vie : %v\nArgent : %v\n", perso.Name, perso.NameUser, perso.Hp, perso.Gold)
+		fmt.Printf("\nVoici les stats du %v %v \nPoints de vie : %v\nArgent : %v\n", perso.Name, perso.NameUser, perso.Hp, perso.Gold)
 	default:
 	}
 	return perso
