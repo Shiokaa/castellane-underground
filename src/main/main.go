a082c526e35d4bdce91cf4311cf7aa863e54f2bb
Amaru Tom <tom.amaru@ynov.com>
2024-09-13 16:25:13 +0200

    fix

--- src/main/main.go
+++ src/main/main.go
@@ -3,22 +3,9 @@ package main
 import (
 	"projet-red/game"
 	"projet-red/menu"
-	"projet-red/story"
 )
 
 func main() {
-<<<<<<< HEAD
-
-	game.HistoireDebut()
 	perso := game.ChoixPersonnage()
-	game.Firstfight(perso)
-=======
-	story.HistoireDebut()
-	perso := game.ChoixPersonnage()
-	story.Afterchoixperso()
-	game.Firstfight(perso)
-	story.Afterguetteur()
 	menu.Menu(perso)
->>>>>>> f13e4f7e053c42328883fddd5af8e8285a81a77c
-
 }
