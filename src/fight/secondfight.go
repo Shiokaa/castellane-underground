84272b7175fe36ac5f17b598a640b1f2ac633947
romain <romain.bourdot@ynov.com>
2024-09-18 16:47:40 +0200

    test

--- src/fight/secondfight.go
+++ src/fight/secondfight.go
@@ -3,20 +3,28 @@ package fight
 import (
 	"fmt"
 	"projet-red/character"
+	"projet-red/game"
 	"time"
 )
 
 func Secondfight(perso character.Personnage) {
 	attack := 0
-	vendeur := character.Enemy{Name: "vandeur", Hp: 200, Damage: 20}
-	fmt.Println("\nTrès bien ! La discussion prend fin et tu décides de te rendre à l’entrée du quartier afin de rencontrer le guetteur pour récolter des informations sur ses supérieurs, mais celui-ci n’est pas ouvert au débat. Le combat commence !")
+	Vendeur := character.Enemy{Name: "Vendeur", Hp: 200, Damage: 20}
+	fmt.Println("\n Vous entrez dans un combat avec le vendeur !")
 	fmt.Println(`
 	   O                         O
 	  /|\                       /|\
 	  / \                       / \`)
-	for vendeur.Hp > 0 && perso.Hp > 0 {
+	time.Sleep(2 * time.Second)
 
-		fmt.Println("\n", perso.NameUser, "a", perso.Hp, "point de vie et ", vendeur.Name, "a", vendeur.Hp, "point de vie")
+	for Vendeur.Hp > 0 && perso.Hp > 0 {
+		fmt.Println("\n--- Combat ---")
+
+		game.DisplayHealth(perso.NameUser, perso.Hp, perso.Hpmax)
+		game.DisplayHealth(Vendeur.Name, Vendeur.Hp, 100)
+		inv.AfficherInventaireEnCombat()
+
+		fmt.Println("\n", perso.NameUser, "a", perso.Hp, "point de vie et ", Vendeur.Name, "a", Vendeur.Hp, "point de vie")
 		fmt.Println("\nAppuyez sur 1 pour lui peter la gueule")
 		fmt.Scan(&attack)
 		for attack != 1 {
@@ -25,20 +33,20 @@ func Secondfight(perso character.Personnage) {
 		}
 
 		switch attack {
-		case 1:
+		case 0:
 			damage := perso.Damage
-			vendeur.Hp -= damage
+			Vendeur.Hp -= damage
 			fmt.Printf("Vous infligez %d points de dégât.\n", damage)
 			time.Sleep(2 * time.Second)
 			fmt.Println("Le vendeur riposte !")
 			time.Sleep(1 * time.Second)
-			perso.Hp -= vendeur.Damage
-			fmt.Printf("Le vendeur vous inflige %d points de dégât.\n", vendeur.Damage)
+			perso.Hp -= Vendeur.Damage
+			fmt.Printf("Le vendeur vous inflige %d points de dégât.\n", Vendeur.Damage)
 			time.Sleep(2 * time.Second)
 
 			if perso.Hp <= 0 {
 				fmt.Println("\nVous êtes tombé au combat...")
-			} else if vendeur.Hp <= 0 {
+			} else if Vendeur.Hp <= 0 {
 				fmt.Println("\nVous avez vaincu le vendeur ! Vous trouvez du tissu et  100 euros !!")
 				/* inv.AddObject(tissu) */
 				perso.Gold += 100
