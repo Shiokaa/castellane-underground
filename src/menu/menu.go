16d61273324dfa4d0073c9e2652fad6708ef93ad
Timothe <timothe.champieux@ynov.com>
2024-09-20 08:35:40 +0200

    fix

--- src/menu/menu.go
+++ src/menu/menu.go
@@ -7,6 +7,7 @@ import (
 	"projet-red/fight"
 	"projet-red/game"
 	"projet-red/inventory"
+	"projet-red/jeutel"
 	"projet-red/object"
 	"time"
 )
@@ -20,6 +21,7 @@ func Menu(perso *character.Personnage, inv inventory.Inventory) {
 		fmt.Println("3 - Aller au O'Tacos (régénération de vie)")
 		fmt.Println("4 - Accéder au Telegram (marché)")
 		fmt.Println("5 - Aller zoner dans le quartier")
+		fmt.Println("6 - Jouez au morpion avec un ami")
 		fmt.Println("0 - Quitter")
 		fmt.Println(" - Vos points de vie ")
 		game.DisplayHealth(perso.NameUser, perso.Hp, perso.Hpmax)
@@ -28,7 +30,7 @@ func Menu(perso *character.Personnage, inv inventory.Inventory) {
 		for {
 			fmt.Printf("Votre choix: ")
 			fmt.Scan(&choix)
-			if choix >= 0 && choix <= 5 {
+			if choix >= 0 && choix <= 6 {
 				break
 			}
 			fmt.Println("Choix invalide, veuillez entrer une valeur valide.")
@@ -86,6 +88,8 @@ func Menu(perso *character.Personnage, inv inventory.Inventory) {
 				} else {
 					fmt.Println("Veuillez combattre le Gérant d'abord")
 				}
+			case 6:
+				jeutel.PlayMorpion()
 			case 0:
 				return
 			}
